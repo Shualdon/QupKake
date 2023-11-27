702930b46aae4a9f9d00dd8a934d4943cfed7315
shualdon <oda6@login0.crc.pitt.edu>
2023-11-27 18:16:19 -0500

    update versioneer

--- qupkake/__init__.py
+++ qupkake/__init__.py
@@ -7,6 +7,5 @@ from .qupkake import *
 print(_version.__file__)
 # from ._version import __version__
 __version__ = _version.get_versions()["version"]
-
 from . import _version
 __version__ = _version.get_versions()['version']
